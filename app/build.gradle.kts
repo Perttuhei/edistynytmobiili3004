9815cfb23f9e7224f684b171df11976ea30999b8
Perttu Heikkala <perttu.heikkala@edu.lapinamk.fi>
2024-04-18 14:08:43 +0300

    add AccountDb, @Entity, @Dao, @Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -50,6 +51,11 @@ android {
 }
 
 dependencies {
+
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    implementation("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     implementation("io.coil-kt:coil-compose:2.5.0")
 
